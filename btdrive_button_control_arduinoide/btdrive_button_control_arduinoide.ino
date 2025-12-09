8a34b0af612f27d62186480512e9ac2dff049521
MrJezKang <markangcog@yahoo.com>
2025-12-09 18:50:40 +0800

    Added Feature: Speed Mode Code Refractor.

--- btdrive_button_control_arduinoide/btdrive_button_control_arduinoide.ino
+++ btdrive_button_control_arduinoide/btdrive_button_control_arduinoide.ino
@@ -1,6 +1,8 @@
 #include "BluetoothSerial.h"
 
-#define Bluetooth "BTCar.Btn.v1.0.0" // Bluetooth Name !
+#define Bluetooth "BTDrive.Button_Control.v1.1.0" // Bluetooth Name !
+
+BluetoothSerial BT;
 
 #define PWMA 27
 #define AIN1 4
@@ -10,41 +12,20 @@
 #define BIN2 32
 #define STBY 33
 
+#define PWM 225
+
 #define Forward  0b10
 #define Backward 0b01
 #define Short    0b11
 #define Close    0b00
 
-#define ledOn 13
-
+uint8_t command = 'S';
+uint8_t defaultSpeed = 0;
 long lastTime = 0;
 
-BluetoothSerial BT;
-
-  // F Foward
-  // B Backward
-  // L Left
-  // R Right
-  // H Forward-Left
-  // G Forward-Right
-  // J Backward-Right
-  // I Backward-Left
-  // S Default BT Value - Stop
-uint8_t MotionTable( uint8_t command ) {
-  switch( command ) {
-    case 'F': return ( Forward  << 2 ) | Forward;
-    case 'B': return ( Backward << 2 ) | Backward;
-    case 'L': return ( Backward << 2 ) | Forward;
-    case 'R': return ( Forward  << 2 ) | Backward;
-    case 'H': return ( Close    << 2 ) | Forward;
-    case 'G': return ( Forward  << 2 ) | Close;
-    case 'J': return ( Close    << 2 ) | Backward;
-    case 'I': return ( Backward << 2 ) | Close;
-    case 'S': return ( Close    << 2 ) | Close;
-    default: return  ( Close    << 2 ) | Close;
-  }
-};
-
+//Bit Packing
+// 0 >> Speed Mode : Switch High to Low Speeds
+uint8_t prerapherals = 0;
 
 void setup() {
   Serial.begin( 115200 );
@@ -58,37 +39,72 @@ void setup() {
   pinMode( BIN2, OUTPUT );
   pinMode( STBY, OUTPUT );
 
-  pinMode( ledOn , OUTPUT );
+  pinMode( 13 , OUTPUT );
 
   digitalWrite( STBY , HIGH );
-  digitalWrite( ledOn , HIGH );
+  digitalWrite( 13 , HIGH );
+  delay( 1000 );
 
 }
 
-uint8_t command = 'S';
-
 void loop() {
-
-  if( millis() - lastTime > 500 ) {
-    lastTime = millis();
-    digitalWrite( ledOn , !digitalRead( ledOn ) ); // blink LED
-  }
 
   // Read incoming Bluetooth data
   if ( BT.available() ) {
     uint8_t data = BT.read();
-        if( data != '\n' && data != '\r' && data != ' ' && data != 0 ) {
-        command = data;
+    if( data != '\n' && data != '\r' && data != ' ' && data != 0 ) {
+
+      if( data == 'Z' ) togglebit( prerapherals , 0 );
+
+      if( data == 'F' || data == 'B' || data == 'L' || data == 'R' ||
+          data == 'H' || data == 'G' || data == 'J' || data == 'I' ||
+          data == 'S' ) command = data;
+
+      digitalWrite( 13 , HIGH ); // turn on LED when data received
+
     }
-  }
-  
+  } else digitalWrite( 13 , LOW ); // turn off LED when no data
+
   uint8_t motor_bit = MotionTable(command);
+  
+  if( getBit( prerapherals , 0 ) ) defaultSpeed = PWM;
+  else defaultSpeed = PWM / 3;
+  
   MotorModuleControl( (motor_bit >> 2) & 0b11 , motor_bit & 0b11 );
 
-  analogWrite( PWMA , 255 );
-  analogWrite( PWMB , 255 );
+  analogWrite( PWMA , defaultSpeed );
+  analogWrite( PWMB , defaultSpeed );
 
 }
+
+  // F Foward
+  // B Backward
+  // L Left
+  // R Right
+  // G Forward-Left
+  // H Forward-Right
+  // I Backward-Right
+  // J Backward-Left
+  // S Default BT Value - Stop
+uint8_t MotionTable( uint8_t command ) {
+  switch( command ) {
+    case 'F': return ( Forward  << 2 ) | Forward;
+    case 'B': return ( Backward << 2 ) | Backward;
+    case 'L': return ( Backward << 2 ) | Forward;
+    case 'R': return ( Forward  << 2 ) | Backward;
+    case 'G': return ( Close    << 2 ) | Forward;
+    case 'H': return ( Forward  << 2 ) | Close;
+    case 'I': return ( Close    << 2 ) | Backward;
+    case 'J': return ( Backward << 2 ) | Close;
+    case 'S': return ( Close    << 2 ) | Close;
+    default: return  ( Close    << 2 ) | Close;
+  }
+};
+
+void setBit(uint8_t &byte, uint8_t bitPos) { byte = byte | 1 << bitPos; }
+void clearBit(uint8_t &byte, uint8_t bitPos) { byte = byte & ~(1 << bitPos); }
+void togglebit(uint8_t &byte, uint8_t bitPos) { byte = byte ^ (1 << bitPos); }
+bool getBit(uint8_t byte, uint8_t bitPos) { return (byte >> bitPos) & 1; }
 
 void MotorModuleControl(uint8_t A, uint8_t B) {
     DirectionControl(A, AIN1, AIN2);
